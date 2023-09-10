fef329647cdbded74ee02c5e96905baf20af4313
Israel <israelamandi@gmail.com>
2023-09-10 15:56:34 +0000

    added mocked db to list userprofile

--- src/main/java/com/aij/crudservice/CrudController.java
+++ src/main/java/com/aij/crudservice/CrudController.java
@@ -4,13 +4,22 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("/")
 public class CrudController {
+
+    private List<UserProfile> db = List.of(new UserProfile("1", "israelatos"));
 
     @GetMapping("/profile")
     public String getProfile() {
         String profile = RandomUsernameGeneration.generateRandomUsername();
         return "Hello, " + profile +"!";
+    }
+
+    @GetMapping("/user")
+    public List<UserProfile> get() {
+        return db;
     }
 }
--- src/main/java/com/aij/crudservice/UserProfile.java
+++ src/main/java/com/aij/crudservice/UserProfile.java
@@ -4,7 +4,17 @@ import lombok.Data;
 
 @Data
 public class UserProfile {
+    public UserProfile() {
+        this.id = id;
+        this.userName = userName;
+    }
+
     private String id;
     private String userName;
 //    other information
+
+    public UserProfile(String id, String userName) {
+        this.id = id;
+        this.userName = userName;
+    }
 }
