b2cd3e586f824d1aa84bb2bd2acd9a65b4354430
Israel <israelamandi@gmail.com>
2023-09-11 02:51:10 +0000

    added -Get User by ID-

--- src/main/java/com/aij/crudservice/CrudController.java
+++ src/main/java/com/aij/crudservice/CrudController.java
@@ -1,16 +1,25 @@
 package com.aij.crudservice;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 
+import java.util.Collection;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/")
 public class CrudController {
 
-    private List<UserProfile> db = List.of(new UserProfile("1", "israelatos"));
+    private Map<String, UserProfile> db = new HashMap<>(){{
+        put("1", new UserProfile("1", "israelatos"));
+    }};
+//    private List<UserProfile> db = List.of();
 
     @GetMapping("/profile")
     public String getProfile() {
@@ -19,7 +28,14 @@ public class CrudController {
     }
 
     @GetMapping("/user")
-    public List<UserProfile> get() {
-        return db;
+    public Collection<UserProfile> get() {
+        return db.values();
+    }
+
+    @GetMapping("/user/{id}")
+    public UserProfile get(@PathVariable String id) {
+        UserProfile userProfile = db.get(id);
+        if (userProfile == null) throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+        return userProfile;
     }
 }
